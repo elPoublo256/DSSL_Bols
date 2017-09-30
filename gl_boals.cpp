4973802123302ad78ede903e3137c3b88b668d7a
elPoublo256 <elpoublo@gmail.com>
2017-09-30 18:01:52 +0300

    AAAA

--- gl_boals.cpp
+++ gl_boals.cpp
@@ -1,8 +1,15 @@
 #include "gl_boals.h"
 #include <math.h>
-double modul( Kordinate &a)
+#include <qalgorithms.h>
+double modulsqr( Kordinate &a)
 {
-  return pow(pow(a.ret_X(),2)+pow(a.ret_Y(),2),0.5);
+  return (pow(a.ret_X(),2)+pow(a.ret_Y(),2));
+}
+double modul(Kordinate &a){return pow(modulsqr(a),0.5);}
+double low_interaction(Kordinate a)
+{
+    ///return (1/a-1/(a*a));
+    return (1/modul(a)-1/modulsqr(a));
 }
 
 GL_Boals::GL_Boals()
@@ -10,3 +17,17 @@ GL_Boals::GL_Boals()
 
 }
 
+Acceleration calc_Acc_One(QVector<GL_Boals *> arr_balls, int i)
+{
+    Kordinate r=arr_balls[i]->ret_Kordinate();
+    arr_balls.remove(i);
+    Acceleration res(0,0);
+    for(GL_Boals* a:arr_balls)
+    {
+        Kordinate dist=r-a->ret_Kordinate();
+        Kordinate norm=dist/modul(dist);
+        res=res+norm*low_interaction(dist);
+
+    }
+    return res;
+}
---
+++ test/test.pro
@@ -0,0 +1,19 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-09-30T18:00:52
+#
+#-------------------------------------------------
+
+QT       += opengl testlib
+
+QT       -= gui
+
+TARGET = tst_testtest
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += tst_testtest.cpp
+DEFINES += SRCDIR=\\\"$$PWD/\\\"
