9ff4428678bf6c5aac228b7bca517e002c1d73a1
elPoublo256 <elpoublo@gmail.com>
2017-10-03 11:28:43 +0200

    BBB

--- gl_boals.cpp
+++ gl_boals.cpp
@@ -6,7 +6,7 @@ double modulsqr( Kordinate &a)
   return (pow(a.ret_X(),2)+pow(a.ret_Y(),2));
 }
 double modul(Kordinate &a){return pow(modulsqr(a),0.5);}
-double low_interaction(Kordinate a)
+double low_interaction(Kordinate& a)
 {
     ///return (1/a-1/(a*a));
     return (1/modul(a)-1/modulsqr(a));
@@ -17,11 +17,12 @@ GL_Boals::GL_Boals()
 
 }
 
-Acceleration calc_Acc_One(QVector<GL_Boals *> arr_balls, int i)
+void calc_Acc_One(vector<GL_Boals *> arr_balls, int i)
 {
     Kordinate r=arr_balls[i]->ret_Kordinate();
-    arr_balls.remove(i);
-    Acceleration res(0,0);
+    auto it=arr_balls.begin()+i;
+
+    Kordinate res(0,0);
     for(GL_Boals* a:arr_balls)
     {
         Kordinate dist=r-a->ret_Kordinate();
@@ -29,5 +30,5 @@ Acceleration calc_Acc_One(QVector<GL_Boals *> arr_balls, int i)
         res=res+norm*low_interaction(dist);
 
     }
-    return res;
+
 }
--- gl_boals.h
+++ gl_boals.h
@@ -1,9 +1,8 @@
 #ifndef GL_BOALS_H
 #define GL_BOALS_H
-#include <QVector>
 #include <omp.h>
-
-
+using namespace std;
+#include <vector>
 class Kordinate
 {
 private:
@@ -18,12 +17,15 @@ void set_Kordinate(const double& X,const double& Y){x_kord=X;y_kord=Y;}
 double ret_Y(){return y_kord;}
 Kordinate operator +(Kordinate b){return Kordinate(this->ret_X()+b.ret_X(),this->ret_Y()+b.ret_Y());}
 Kordinate operator -(Kordinate b){return Kordinate(this->ret_X()-b.ret_X(),this->ret_Y()-b.ret_Y());}
-Kordinate operator *(double b){return Kordinate(this->ret_X()*b,this->ret_Y()*b);}
-Kordinate operator /(double b){return Kordinate(this->ret_X()/b,this->ret_Y()/b);}
+
 //можно было бы вообще использовать Eigen::vector2D или uBlas какой нить, но я поздно спохватился
 //bool operator ==(Kordinate b){return (this->ret_X()==b.ret_X())&&(this->ret_Y()==b.ret_Y());}
 };
-
+template <class T>
+Kordinate operator *(Kordinate a, T b){return Kordinate(a.ret_X()*b,a.ret_Y()*b);}
+template <class T>
+Kordinate operator /(Kordinate a, T b){return Kordinate(a.ret_X()/b,a.ret_Y()/b);}
+/*
 class Velocity : public Kordinate
 {
 public:
@@ -39,6 +41,7 @@ public:
     Acceleration(const int& x, const int& y) : Kordinate(x,y){}
 
 };
+*/
 double modulsqr( Kordinate& a);
 double modul( Kordinate& a);
 
@@ -47,26 +50,26 @@ class GL_Boals
 private:
 
     double size;
-    Kordinate kor;
-    Velocity v;
-    Acceleration a;
+    Kordinate kordinate;
+    Kordinate velocity;
+    Kordinate acceleration;
 
 public:
     GL_Boals();
-    GL_Boals(const Kordinate& r, const Velocity& v, const Acceleration& a);
-    void set_kor(const Kordinate& r){kor=r;}
-    void set_Velocity(const Velocity& r){v=r;}
-    void set_acceleration(const Acceleration& r){a=r;}
+    GL_Boals(const Kordinate& r, const Kordinate& v, const Kordinate& a);
+    void set_kor(const Kordinate& r){kordinate=r;}
+    void set_Velocity(const Kordinate& r){velocity=r;}
+    void set_acceleration(const Kordinate& r){acceleration=r;}
     void set_size(const double& s){size=s;}
-    Kordinate ret_Kordinate(){return kor;}
-    Kordinate ret_Velocity(){return v;}
-    Kordinate ret_Acceleration(){return a;}
+    Kordinate ret_Kordinate(){return kordinate;}
+    Kordinate ret_Velocity(){return velocity;}
+    Kordinate ret_Acceleration(){return acceleration;}
 };
-double low_interaction(Kordinate a);
+double low_interaction(Kordinate& a);
 ///This function calculates the acceleration for array_balls[i]  in the array_balls
-Acceleration calc_Acc_One(QVector<GL_Boals*> arr_balls, int i);
+void calc_Acc_One(vector<GL_Boals*> arr_balls, int i);
 ///This function calculates and sets the acceleration for each ball in the array_balls
-void calc_Acc_ALL(QVector<GL_Boals*> arr_balls);
+
 
 
 
