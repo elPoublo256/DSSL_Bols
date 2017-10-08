6122e0eea9b1f8baebe763ffb50b77033adea9f6
elPoublo256 <elpoublo@gmail.com>
2017-10-08 10:42:44 +0200

    BBB

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "widget.h"
 #include <QApplication>
-
+#include <myglwidget.h>
+#include <GL/glu.h>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Widget w;
+    //Widget
+    MyGLWidget w;
     w.show();
+    w.update();
 
     return a.exec();
 }
--- myglwidget.h
+++ myglwidget.h
@@ -1,16 +1,25 @@
 #ifndef MYGLWIDGET_H
 #define MYGLWIDGET_H
 #include <QGLWidget>
-
+#include <GL/glu.h>
+#include <gl_boals.h>
 class MyGLWidget : public QGLWidget
 {
-   protected:
-    virtual void initializeGL();
-    virtual void resizeGL(int w, int h);
-    virtual void paintGL();
+private:
+    GLUquadricObj* gluobj;
+    int num_B;
+    vector<GL_Boals>* vB;
+    void show_boals();
+public:
+    void initializeGL();
+    void resizeGL(int w, int h);
+    void paintGL();
+
 public:
     MyGLWidget();
+    MyGLWidget(int num);
 
 };
+
 
 #endif // MYGLWIDGET_H
